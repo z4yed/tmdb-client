98945cdfe884c6684145625979880da779b99670
Zayed <zayed.cs3@gmail.com>
2024-01-07 00:05:40 +0600

    Static hero section completed

---
+++ src/components/templates/Header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <Navbar />
+    <Hero />
+  </div>
+</template>
+
+<script>
+import { Navbar, Hero } from "@/components/organisms";
+
+export default {
+  components: {
+    Navbar,
+    Hero,
+  },
+};
+</script>
+
+<style lang="scss">
+.header {
+  position: relative;
+  min-height: 648px;
+}
+</style>
