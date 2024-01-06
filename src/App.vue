0ccad25fb17c77504fe01a066dcba5f7e2f1e346
Zayed <zayed.cs3@gmail.com>
2024-01-06 14:26:05 +0600

    Rubik font imported from google fonts

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap");
+
 body {
   background: $color-black-solid;
   color: white;
